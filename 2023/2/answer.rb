b76d85da5c0fc367c9f1e00edf622aba8f5b5da9
Tanner Mares <tannermares@gmail.com>
2023-12-02 17:27:51 -0800

    got part 2 working

--- 2023/2/answer.rb
+++ 2023/2/answer.rb
@@ -26,4 +26,29 @@ part1 = input.sum do |row|
   invalid_game ? 0 : game_id.to_i
 end
 
+input.close
+input = File.open(File.join(File.dirname(__FILE__), 'input.txt'))
+
+part2 = input.sum do |row|
+  _title, game = row.split(':')
+  sets = game.split(';')
+  current_mins = {
+    'blue' => 0,
+    'green' => 0,
+    'red' => 0
+  }
+
+  sets.each do |set|
+    cubes = set.split(', ')
+
+    cubes.each do |cube|
+      pulled, color = cube.split(' ')
+      current_mins[color] = pulled.to_i if current_mins[color] <= pulled.to_i
+    end
+  end
+
+  current_mins.values.reduce(:*)
+end
+
 puts "Part 1 Answer: #{part1}"
+puts "Part 2 Answer: #{part2}"
