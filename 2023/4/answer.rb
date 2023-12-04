b1d5b639a7edd1a707b25d20524e7a78c164c268
Tanner Mares <tannermares@gmail.com>
2023-12-04 07:45:01 -0800

    added other boilerplate

---
+++ 2023/4/answer.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+INPUT_PATH = File.join(File.dirname(__FILE__), 'sample.txt').freeze
+INPUT = File.readlines(INPUT_PATH)
+
+def part1
+  INPUT.sum do |row|
+  end
+end
+
+def part2
+  INPUT.sum do |row|
+  end
+end
+
+puts "Part 1 Answer: #{part1}"
+puts "Part 2 Answer: #{part2}"
---
+++ 2023/4/answer_test.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'test/unit'
+require 'stringio'
+require_relative 'answer'
+
+class TestDay3 < Test::Unit::TestCase
+  def test_part1
+    assert_equal(13, part1)
+  end
+
+  # def test_part2
+  #   assert_equal(82_301_120, part2)
+  # end
+end
