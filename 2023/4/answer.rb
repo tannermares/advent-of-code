5d3f60577e70232607cf82ae634467b45c2e3644
Tanner Mares <tannermares@gmail.com>
2023-12-04 15:39:56 -0800

    got day 4 part 1 working

--- 2023/4/answer.rb
+++ 2023/4/answer.rb
@@ -1,16 +1,28 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-INPUT_PATH = File.join(File.dirname(__FILE__), 'sample.txt').freeze
+INPUT_PATH = File.join(File.dirname(__FILE__), 'input.txt').freeze
 INPUT = File.readlines(INPUT_PATH)
 
 def part1
   INPUT.sum do |row|
+    _card_number, numbers = row.split(': ')
+    winning_numbers, my_numbers = numbers.split(' | ').map(&:split)
+    matches = (winning_numbers & my_numbers).length
+
+    if matches.positive?
+      score = 1
+      (matches - 1).times { score *= 2 }
+      score
+    else
+      0
+    end
   end
 end
 
 def part2
   INPUT.sum do |row|
+    0
   end
 end
 
