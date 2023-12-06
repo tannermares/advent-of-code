da24de7fa929aebbf3af47bea29208277afc02cf
Tanner Mares <tannermares@gmail.com>
2023-12-05 20:49:13 -0800

    simplified part 1 a little

--- 2023/5/answer_test.rb
+++ 2023/5/answer_test.rb
@@ -7,10 +7,10 @@ require_relative 'answer'
 
 class TestDay3 < Test::Unit::TestCase
   def test_part1
-    assert_equal(35, part1)
+    assert_equal(510_109_797, part1)
   end
 
-  # def test_part2
-  #   assert_equal(6_227_972, part2)
-  # end
+  def test_part2
+    assert_equal(46, part2)
+  end
 end
