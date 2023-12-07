20f54bd37113dcabd80de919a800b3e8d50e51b3
Tanner Mares <tannermares@gmail.com>
2023-12-06 18:02:40 -0800

    trying part 2

--- 2023/5/answer.rb
+++ 2023/5/answer.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-INPUT_PATH = File.join(File.dirname(__FILE__), 'input.txt').freeze
+INPUT_PATH = File.join(File.dirname(__FILE__), 'sample.txt').freeze
 INPUT = File.readlines(INPUT_PATH)
 
 def part1
@@ -46,27 +46,96 @@ def part2
     current_map += 1 and next if row.include?('map:')
     next if row == "\n"
 
-    dest, source, length = row.split
-
-    maps[current_map] ||= { destinations: [], lengths: [], sources: [] }
-    maps[current_map][:destinations] << dest.to_i
-    maps[current_map][:sources] << source.to_i
-    maps[current_map][:lengths] << length.to_i
+    dest, source, length = row.split.map(&:to_i)
+    maps[current_map] ||= []
+    maps[current_map] << [source, source + length - 1, dest - source]
   end
 
-  locations = []
-  # seeds.each_slice(2) do |seed, count|
-  #   seed_range = [seed, seed + count]
-  #   maps.each do |map|
-  #     map_value = seed
-  #     map[:sources].each_with_index do |source, index|
-  #       source_range = [source, source + map[:lengths][index]]
-  #       adjustment = map[:destinations][index] - source
-  #     end
-  #   end
-  # end
+  # seeds
+  # [[79, 92], [55, 67]]
 
-  locations.flatten.min
+  # seed to soil
+  # 98-99 -> -48
+  # 50-97 -> +2
+  #------------
+  # output: [[81,94], [57..69]]
+
+  # soil-to-fert
+  # 15-51 -> -15
+  # 52-53 -> -15
+  # 0-14  -> +39
+  #------------
+  # output: [(81..94), (57..69)]
+
+  # fert-to-water
+  # 53-60 -> -4
+  # 11-52 -> -11
+  # 0-6   -> 42
+  # 7-10  -> 50
+  #-----------
+  # (57..60) -4
+  # (61..69) 0
+  # output: [(81..94), (53..56), (61..69)]
+
+  # water-to-light
+  # 18-24 -> 70
+  # 25-94 -> -7
+  #------------
+  # (81..94) -7
+  # (53..56) -7
+  # (61..69) -7
+  # output: [(74..87), (46..49), (54..62)]
+
+  # light-to-temperature
+  # 77-99 -> -32
+  # 45-63 -> 36
+  # 64-76 -> 4
+  #-------------
+  # (74..76) +4
+  # (77..87) -32
+  # (46..49) +36
+  # (54..62) +36
+  # output: [(78..80), (45..55), (82..85), (90..98)]
+
+  # temp-to-hum
+  # 69   -> -69
+  # 0-68 -> +1
+  #-----------
+  # (78..80) 0
+  # (45..55) +1
+  # (82..85) 0
+  # (90..98) 0
+  # output: [(78..80), (46..56), (82..85), (90..98)]
+
+  # hum-to-loc
+  # 56-92 -> +4
+  # 93-96 -> -37
+  #-----------
+  # (78..80) +4
+  # (46..55) 0
+  # (56)     +4
+  # (82..85) +4
+  # (90..92) +4
+  # (93..96) -37
+  # (97..98) 0
+  # output: [(82..84), (46..55), (60), (86..89), (94..96), (56..59), (97..98)]
+
+  # you wind up building a source range into one or more destination ranges as you apply each entry in the map.
+
+  ranges = []
+  seeds.each_slice(2) { |seed, count| ranges << [seed, seed + count] }
+
+  maps.each do |map|
+    # adjustment = [[81, 94], [53, 56], (61..69)]
+    map.each do |adjustment|
+      # ranges -> [[79, 92], [55, 67]]
+      ranges.each do |range|
+        # potentially return multiple arrays
+      end
+    end
+  end
+
+  ranges.flatten.min
 end
 
 puts "Part 1 Answer: #{part1}"
--- 2023/5/answer_test.rb
+++ 2023/5/answer_test.rb
@@ -6,9 +6,9 @@ require 'stringio'
 require_relative 'answer'
 
 class TestDay3 < Test::Unit::TestCase
-  def test_part1
-    assert_equal(510_109_797, part1)
-  end
+  # def test_part1
+  #   assert_equal(510_109_797, part1)
+  # end
 
   def test_part2
     assert_equal(46, part2)
