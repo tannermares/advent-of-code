76f04fcbbd8227a7a5eff48e9a305adcafa99b58
Tanner Mares <tannermares@gmail.com>
2023-12-08 09:29:22 -0800

    got part 1

--- 2023/8/answer.rb
+++ 2023/8/answer.rb
@@ -1,16 +1,39 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-INPUT_PATH = File.join(File.dirname(__FILE__), 'input.txt').freeze
+SAMPLE = true
+INPUT_PATH = File.join(File.dirname(__FILE__), SAMPLE ? 'sample.txt' : 'input.txt').freeze
 INPUT = File.readlines(INPUT_PATH)
 
 def part1
+  steps = 0
+  nodes = {}
+  instructions = nil
+  current_node = 'AAA'
 
+  INPUT.each_with_index do |row, index|
+    instructions = row.strip and next if index.zero?
+    next if row == "\n"
+
+    start, next_step = row.split(' = ')
+    next_step = next_step.gsub(/\(|\)/, '').strip.split(', ')
+
+    puts start
+    puts next_step.inspect
+
+    nodes[start] = next_step
+  end
+
+  while current_node != 'ZZZ'
+    instructions.each_char.each do |inst|
+      steps += 1
+      current_node = inst == 'L' ? nodes[current_node][0] : nodes[current_node][1]
+    end
+  end
+
+  steps
 end
 
 def part2
 
 end
-
-puts "Part 1 Answer: #{part1}"
-puts "Part 2 Answer: #{part2}"
--- 2023/8/answer_test.rb
+++ 2023/8/answer_test.rb
@@ -7,10 +7,10 @@ require_relative 'answer'
 
 class TestDay7 < Test::Unit::TestCase
   def test_part1
-    assert_equal(6, part1)
+    SAMPLE ? assert_equal(6, part1) : assert_equal(13_301, part1)
   end
 
-  # def test_part2
-  #   assert_equal(0, part2)
-  # end
+  def test_part2
+    SAMPLE ? assert_equal(6, part2) : assert_equal(0, part2)
+  end
 end
