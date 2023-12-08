365162e22bb6a11a2871266c7fc4ea6b9d61fb8e
Tanner Mares <tannermares@gmail.com>
2023-12-08 08:12:44 -0800

    got part 2

--- 2023/7/answer.rb
+++ 2023/7/answer.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-INPUT_PATH = File.join(File.dirname(__FILE__), 'sample.txt').freeze
+INPUT_PATH = File.join(File.dirname(__FILE__), 'input.txt').freeze
 INPUT = File.readlines(INPUT_PATH)
 
-RANKS = {
+NORMAL_RANKS = {
   'A' => 14,
   'K' => 13,
   'Q' => 12,
@@ -19,7 +19,21 @@ RANKS = {
   '3' => 3,
   '2' => 2
 }.freeze
-TYPES = ['five of a kind', 'four of a kind', 'full house', 'three of a kind', 'two pair', 'one pair', 'high card'].reverse
+JOKER_RANKS = {
+  'A' => 14,
+  'K' => 13,
+  'Q' => 12,
+  'T' => 10,
+  '9' => 9,
+  '8' => 8,
+  '7' => 7,
+  '6' => 6,
+  '5' => 5,
+  '4' => 4,
+  '3' => 3,
+  '2' => 2,
+  'J' => 1
+}.freeze
 
 def type_hand(hand)
   sorted_hand = hand.each_char.sort
@@ -46,28 +60,71 @@ def type_hand(hand)
   elsif sorted_hand[0] != sorted_hand[1] && sorted_hand[1] != sorted_hand[2] && sorted_hand[2] != sorted_hand[3] && sorted_hand[03] != sorted_hand[4]
     0
   else
-    raise 'WAT'
+    raise "Invalid type for: #{sorted_hand}"
   end
 end
 
-def type_hands(hands_with_scores)
+def type_hand_with_jokers(hand)
+  # Sort Jokers to the bottom
+  sorted_hand = hand.each_char.sort_by { |s| s =~ /^J$/ ? [2, Regexp.last_match[0]] : [1, s] }
+  number_of_jokers = sorted_hand.count { |a| a == 'J' }
+
+  if sorted_hand[0] == sorted_hand[3] && number_of_jokers == 1 ||
+     sorted_hand[0] == sorted_hand[2] && number_of_jokers == 2 ||
+     sorted_hand[0] == sorted_hand[1] && number_of_jokers == 3 ||
+     number_of_jokers == 4 ||
+     number_of_jokers == 5
+    6
+  elsif sorted_hand[0] == sorted_hand[2] && number_of_jokers == 1 && sorted_hand[2] != sorted_hand[3] ||
+        sorted_hand[0] != sorted_hand[1] && number_of_jokers == 1 && sorted_hand[1] == sorted_hand[3] ||
+        sorted_hand[0] == sorted_hand[1] && number_of_jokers == 2 && sorted_hand[1] != sorted_hand[2] ||
+        sorted_hand[0] != sorted_hand[1] && number_of_jokers == 2 && sorted_hand[1] == sorted_hand[2] ||
+        number_of_jokers == 3 && sorted_hand[0] != sorted_hand[1]
+    5
+  elsif sorted_hand[0] == sorted_hand[1] && sorted_hand[1] != sorted_hand[2] && sorted_hand[2] == sorted_hand[3] && number_of_jokers == 1
+    4
+  elsif sorted_hand[0] == sorted_hand[1] && sorted_hand[1] != sorted_hand[2] && sorted_hand[2] != sorted_hand[3] && number_of_jokers == 1 ||
+        sorted_hand[0] != sorted_hand[1] && sorted_hand[1] == sorted_hand[2] && sorted_hand[2] != sorted_hand[3] && number_of_jokers == 1 ||
+        sorted_hand[0] != sorted_hand[1] && sorted_hand[1] != sorted_hand[2] && sorted_hand[2] == sorted_hand[3] && number_of_jokers == 1 ||
+        sorted_hand[0] != sorted_hand[1] && sorted_hand[1] != sorted_hand[2] && sorted_hand[2] != sorted_hand[3] && number_of_jokers == 2
+    3
+  elsif sorted_hand[0] != sorted_hand[1] && sorted_hand[1] != sorted_hand[2] && sorted_hand[2] != sorted_hand[3] && sorted_hand[03] != sorted_hand[4] && number_of_jokers == 1
+    1
+  else
+    raise "Invalid type for: #{sorted_hand}"
+  end
+end
+
+def type_hands(hands_with_scores, with_wilds: false)
   hands_with_scores.reduce([]) do |acc, hand_with_score|
-    type = type_hand(hand_with_score[0])
+    type = if with_wilds && hand_with_score[0].include?('J')
+      type_hand_with_jokers(hand_with_score[0])
+    else
+      type_hand(hand_with_score[0])
+    end
+
     acc[type] ||= []
     acc[type] << hand_with_score
     acc
   end
 end
 
-def sort_hands(typed_with_scores)
+def sort_hands(typed_with_scores, ranks: NORMAL_RANKS)
   typed_with_scores.map do |typed_hands_with_scores|
     next unless typed_hands_with_scores
+
     typed_hands_with_scores.sort do |a, b|
       first_diff_index = 0
       first_diff_index += 1 while a[0][first_diff_index] == b[0][first_diff_index]
 
-      RANKS[a[0][first_diff_index]] <=> RANKS[b[0][first_diff_index]]
+      ranks[a[0][first_diff_index]] <=> ranks[b[0][first_diff_index]]
     end
+  end
+end
+
+def sum_ranks(sorted_typed_with_scores)
+  sorted_typed_with_scores.compact.flatten.each_slice(2).with_index.sum do |(_hand, score), rank|
+    score.to_i * (rank + 1)
   end
 end
 
@@ -75,20 +132,15 @@ def part1
   hands_with_scores = INPUT.map(&:split)
   typed_with_scores = type_hands(hands_with_scores)
   sorted_typed_with_scores = sort_hands(typed_with_scores)
-  # puts '-' * 50
-  # sorted_typed_with_scores.each_with_index do |hands, index|
-  #   puts TYPES[index]
-  #   puts hands.inspect
-  # end
-  # puts sorted_typed_with_scores.flatten.inspect
-  # puts '-' * 50
-  sorted_typed_with_scores.compact.flatten.each_slice(2).with_index.sum do |(_hand, score), rank|
-    score.to_i * (rank + 1)
-  end
+  sum_ranks(sorted_typed_with_scores)
 end
 
 def part2
+  hands_with_scores = INPUT.map(&:split)
+  typed_with_scores = type_hands(hands_with_scores, with_wilds: true)
+  sorted_typed_with_scores = sort_hands(typed_with_scores, ranks: JOKER_RANKS)
+  sum_ranks(sorted_typed_with_scores)
 end
 
 puts "Part 1 Answer: #{part1}"
-# puts "Part 2 Answer: #{part2}"
+puts "Part 2 Answer: #{part2}"
--- 2023/7/answer_test.rb
+++ 2023/7/answer_test.rb
@@ -7,29 +7,78 @@ require_relative 'answer'
 
 class TestDay3 < Test::Unit::TestCase
   def test_type_hand
+    # Five of a kind
     assert_equal(6, type_hand('AAAAA'))
+
+    # Four of a kind
     assert_equal(5, type_hand('AAAAB'))
     assert_equal(5, type_hand('ABBBB'))
+
+    # Full House
     assert_equal(4, type_hand('AAABB'))
     assert_equal(4, type_hand('AABBB'))
+
+    # Three of a kind
     assert_equal(3, type_hand('AAABC'))
     assert_equal(3, type_hand('ABCCC'))
     assert_equal(3, type_hand('ABBBC'))
+
+    # Two Pair
     assert_equal(2, type_hand('AABBC'))
     assert_equal(2, type_hand('ABBCC'))
     assert_equal(2, type_hand('AABCC'))
+
+    # One Pair
     assert_equal(1, type_hand('AABCD'))
     assert_equal(1, type_hand('ABBCD'))
     assert_equal(1, type_hand('ABCCD'))
     assert_equal(1, type_hand('ABCDD'))
+
+    # High Card
     assert_equal(0, type_hand('ABCDE'))
   end
 
+  def test_type_hand_with_jokers
+    # Five of a kind
+    assert_equal(6, type_hand_with_jokers('AAAAJ'))
+    assert_equal(6, type_hand_with_jokers('AAAJJ'))
+    assert_equal(6, type_hand_with_jokers('AAJJJ'))
+    assert_equal(6, type_hand_with_jokers('AJJJJ'))
+    assert_equal(6, type_hand_with_jokers('JJJJJ'))
+
+    # Four of a kind
+    assert_equal(5, type_hand_with_jokers('AAABJ'))
+    assert_equal(5, type_hand_with_jokers('ABBBJ'))
+    assert_equal(5, type_hand_with_jokers('AABJJ'))
+    assert_equal(5, type_hand_with_jokers('ABBJJ'))
+    assert_equal(5, type_hand_with_jokers('ABJJJ'))
+
+    # Full house
+    assert_equal(4, type_hand_with_jokers('AABBJ'))
+
+    # Three of a kind
+    assert_equal(3, type_hand_with_jokers('AABCJ'))
+    assert_equal(3, type_hand_with_jokers('ABCCJ'))
+    assert_equal(3, type_hand_with_jokers('ABBCJ'))
+    assert_equal(3, type_hand_with_jokers('ABCJJ'))
+
+    # Two Pair
+    # Not possible with a joker
+
+    # One Pair
+    assert_equal(1, type_hand_with_jokers('ABCDJ'))
+
+    # High Card
+    # Not possible with a joker
+  end
+
   def test_part1
-    assert_equal(250_898_830, part1)
+    assert_equal(250_898_830, part1) # Input
+    # assert_equal(6_440, part1) # Sample
   end
 
   def test_part2
-    assert_equal(5905, part2)
+    assert_equal(252_127_335, part2) # Input
+    # assert_equal(5_905, part2) # Sample
   end
 end
