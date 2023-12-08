2565282a8ba9d59f85a94d676ff753726fa12d0b
Tanner Mares <tannermares@gmail.com>
2023-12-07 20:34:36 -0800

    got part 1

--- 2023/7/answer.rb
+++ 2023/7/answer.rb
@@ -4,8 +4,87 @@
 INPUT_PATH = File.join(File.dirname(__FILE__), 'sample.txt').freeze
 INPUT = File.readlines(INPUT_PATH)
 
+RANKS = {
+  'A' => 14,
+  'K' => 13,
+  'Q' => 12,
+  'J' => 11,
+  'T' => 10,
+  '9' => 9,
+  '8' => 8,
+  '7' => 7,
+  '6' => 6,
+  '5' => 5,
+  '4' => 4,
+  '3' => 3,
+  '2' => 2
+}.freeze
+TYPES = ['five of a kind', 'four of a kind', 'full house', 'three of a kind', 'two pair', 'one pair', 'high card'].reverse
+
+def type_hand(hand)
+  sorted_hand = hand.each_char.sort
+
+  if sorted_hand[0] == sorted_hand[4]
+    6
+  elsif sorted_hand[0] == sorted_hand[3] || sorted_hand[1] == sorted_hand[4]
+    5
+  elsif sorted_hand[0] == sorted_hand[2] && sorted_hand[3] == sorted_hand[4] || sorted_hand[0] == sorted_hand[1] && sorted_hand[2] == sorted_hand[4]
+    4
+  elsif sorted_hand[0] == sorted_hand[2] && sorted_hand[3] != sorted_hand[4] || 
+        sorted_hand[0] != sorted_hand[1] && sorted_hand[2] == sorted_hand[4] ||
+        sorted_hand[0] != sorted_hand[1] && sorted_hand[1] == sorted_hand[3] && sorted_hand[3] != sorted_hand[4]
+    3
+  elsif sorted_hand[0] == sorted_hand[1] && sorted_hand[1] != sorted_hand[2] && sorted_hand[2] == sorted_hand[3] && sorted_hand[03] != sorted_hand[4] || 
+        sorted_hand[0] != sorted_hand[1] && sorted_hand[1] == sorted_hand[2] && sorted_hand[2] != sorted_hand[3] && sorted_hand[03] == sorted_hand[4] ||
+        sorted_hand[0] == sorted_hand[1] && sorted_hand[1] != sorted_hand[2] && sorted_hand[2] != sorted_hand[3] && sorted_hand[03] == sorted_hand[4]
+    2
+  elsif sorted_hand[0] == sorted_hand[1] && sorted_hand[1] != sorted_hand[2] && sorted_hand[2] != sorted_hand[3] && sorted_hand[03] != sorted_hand[4] || 
+        sorted_hand[0] != sorted_hand[1] && sorted_hand[1] == sorted_hand[2] && sorted_hand[2] != sorted_hand[3] && sorted_hand[03] != sorted_hand[4] ||
+        sorted_hand[0] != sorted_hand[1] && sorted_hand[1] != sorted_hand[2] && sorted_hand[2] == sorted_hand[3] && sorted_hand[03] != sorted_hand[4] ||
+        sorted_hand[0] != sorted_hand[1] && sorted_hand[1] != sorted_hand[2] && sorted_hand[2] != sorted_hand[3] && sorted_hand[03] == sorted_hand[4]
+    1
+  elsif sorted_hand[0] != sorted_hand[1] && sorted_hand[1] != sorted_hand[2] && sorted_hand[2] != sorted_hand[3] && sorted_hand[03] != sorted_hand[4]
+    0
+  else
+    raise 'WAT'
+  end
+end
+
+def type_hands(hands_with_scores)
+  hands_with_scores.reduce([]) do |acc, hand_with_score|
+    type = type_hand(hand_with_score[0])
+    acc[type] ||= []
+    acc[type] << hand_with_score
+    acc
+  end
+end
+
+def sort_hands(typed_with_scores)
+  typed_with_scores.map do |typed_hands_with_scores|
+    next unless typed_hands_with_scores
+    typed_hands_with_scores.sort do |a, b|
+      first_diff_index = 0
+      first_diff_index += 1 while a[0][first_diff_index] == b[0][first_diff_index]
+
+      RANKS[a[0][first_diff_index]] <=> RANKS[b[0][first_diff_index]]
+    end
+  end
+end
+
 def part1
-  INPUT
+  hands_with_scores = INPUT.map(&:split)
+  typed_with_scores = type_hands(hands_with_scores)
+  sorted_typed_with_scores = sort_hands(typed_with_scores)
+  # puts '-' * 50
+  # sorted_typed_with_scores.each_with_index do |hands, index|
+  #   puts TYPES[index]
+  #   puts hands.inspect
+  # end
+  # puts sorted_typed_with_scores.flatten.inspect
+  # puts '-' * 50
+  sorted_typed_with_scores.compact.flatten.each_slice(2).with_index.sum do |(_hand, score), rank|
+    score.to_i * (rank + 1)
+  end
 end
 
 def part2
--- 2023/7/answer_test.rb
+++ 2023/7/answer_test.rb
@@ -6,11 +6,30 @@ require 'stringio'
 require_relative 'answer'
 
 class TestDay3 < Test::Unit::TestCase
+  def test_type_hand
+    assert_equal(6, type_hand('AAAAA'))
+    assert_equal(5, type_hand('AAAAB'))
+    assert_equal(5, type_hand('ABBBB'))
+    assert_equal(4, type_hand('AAABB'))
+    assert_equal(4, type_hand('AABBB'))
+    assert_equal(3, type_hand('AAABC'))
+    assert_equal(3, type_hand('ABCCC'))
+    assert_equal(3, type_hand('ABBBC'))
+    assert_equal(2, type_hand('AABBC'))
+    assert_equal(2, type_hand('ABBCC'))
+    assert_equal(2, type_hand('AABCC'))
+    assert_equal(1, type_hand('AABCD'))
+    assert_equal(1, type_hand('ABBCD'))
+    assert_equal(1, type_hand('ABCCD'))
+    assert_equal(1, type_hand('ABCDD'))
+    assert_equal(0, type_hand('ABCDE'))
+  end
+
   def test_part1
-    assert_equal(800_280, part1)
+    assert_equal(250_898_830, part1)
   end
 
   def test_part2
-    assert_equal(45_128_024, part2)
+    assert_equal(5905, part2)
   end
 end
