cb792a81048eb09b12d8e18901bc39191f667201
Tanner Mares <tannermares@gmail.com>
2023-12-09 09:10:32 -0800

    added day 9 boilerplate

---
+++ 2023/9/answer.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+SAMPLE = true
+INPUT_PATH = File.join(File.dirname(__FILE__), SAMPLE ? 'sample.txt' : 'input.txt').freeze
+INPUT = File.readlines(INPUT_PATH)
+
+def part1
+  
+end
+
+def part2
+
+end
---
+++ 2023/9/answer_test.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'test/unit'
+require 'stringio'
+require_relative 'answer'
+
+class TestDay9 < Test::Unit::TestCase
+  def test_part1
+    SAMPLE ? assert_equal(114, part1) : assert_equal(0, part1)
+  end
+
+  # def test_part2
+  #   SAMPLE ? assert_equal(0, part2) : assert_equal(0, part2)
+  # end
+end
