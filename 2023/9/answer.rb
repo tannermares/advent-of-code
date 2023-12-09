8d3cd23b38afcc923dec8581416ec145cbcd8a43
Tanner Mares <tannermares@gmail.com>
2023-12-09 11:13:16 -0800

    got part 1

--- 2023/9/answer.rb
+++ 2023/9/answer.rb
@@ -6,9 +6,54 @@ INPUT_PATH = File.join(File.dirname(__FILE__), SAMPLE ? 'sample.txt' : 'input.tx
 INPUT = File.readlines(INPUT_PATH)
 
 def part1
-  
+  INPUT.each_with_index.sum do |row, index|
+    steps[index] ||= []
+    steps[index] << row.split.map(&:to_i)
+
+    until steps[index].last.uniq == [0]
+      last_step = steps[index].last
+      next_step = last_step.map.with_index { |n, i| i + 1 < last_step.length ? last_step[i + 1] - n : nil }.compact
+      steps[index] << next_step
+    end
+
+    reversed = steps[index].reverse
+    reversed.each_with_index do |step, i|
+      step << 0
+      previous_row = reversed[i - 1] unless i.zero?
+      step[-1] = step[-2] + previous_row[-1] if previous_row
+    end
+
+    steps[index].first.last
+  end
 end
 
 def part2
+  INPUT.each_with_index.sum do |row, index|
+    steps[index] ||= []
+    steps[index] << row.split.map(&:to_i)
 
+    until steps[index].last.uniq == [0]
+      last_step = steps[index].last
+      next_step = last_step.map.with_index { |n, i| i + 1 < last_step.length ? last_step[i + 1] - n : nil }.compact
+      steps[index] << next_step
+    end
+
+    reversed = steps[index].reverse
+    reversed.each_with_index do |step, i|
+      step << 0
+      previous_row = reversed[i - 1] unless i.zero?
+
+      # puts '-' * 50
+      # puts "INDEX: #{i}"
+      # puts "Step: #{step.inspect}"
+      # puts "Previous Row: #{previous_row.inspect}"
+      # puts "previous item: #{step[-2]}"
+      # puts "adding: #{previous_row[-1]}" if previous_row
+      # puts '-' * 50
+
+      step[-1] = step[-2] + previous_row[-1] if previous_row
+    end
+
+    steps[index].first.last
+  end
 end
--- 2023/9/answer_test.rb
+++ 2023/9/answer_test.rb
@@ -7,10 +7,10 @@ require_relative 'answer'
 
 class TestDay9 < Test::Unit::TestCase
   def test_part1
-    SAMPLE ? assert_equal(114, part1) : assert_equal(0, part1)
+    SAMPLE ? assert_equal(114, part1) : assert_equal(2_043_677_056, part1)
   end
 
-  # def test_part2
-  #   SAMPLE ? assert_equal(0, part2) : assert_equal(0, part2)
-  # end
+  def test_part2
+    SAMPLE ? assert_equal(2, part2) : assert_equal(0, part2)
+  end
 end
