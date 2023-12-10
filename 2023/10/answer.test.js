c0a7be61699114802dd24bcd63dc3645d4866628
Tanner Mares <tannermares@gmail.com>
2023-12-10 10:07:36 -0800

    skip day 10 tests temp

--- 2023/10/answer.test.js
+++ 2023/10/answer.test.js
@@ -5,12 +5,15 @@ const assert = require('assert')
 const { part1, part2, SAMPLE } = require('./answer.js')
 
 describe('day10', () => {
-  test('part1', () => {
+  test('part1', (t) => {
+    t.skip()
+    return
     SAMPLE ? assert.strictEqual(8, part1()) : assert.strictEqual(null, part1())
   })
 
   test('part2', (t) => {
     t.skip()
+    return
     SAMPLE ? assert.strictEqual(2, part2()) : assert.strictEqual(1_062, part2())
   })
 })
