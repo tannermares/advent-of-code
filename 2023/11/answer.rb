5a196916988054efd43cb7daaa0e5bdfd214ed93
Tanner Mares <tannermares@gmail.com>
2023-12-14 15:27:53 -0800

    started day 11

--- 2023/11/answer.rb
+++ 2023/11/answer.rb
@@ -6,10 +6,23 @@ module Day11
   SAMPLE = true
   INPUT_PATH = File.join(File.dirname(__FILE__), SAMPLE ? 'sample.txt' : 'input.txt').freeze
   INPUT = File.readlines(INPUT_PATH)
+  EMPTY_SPACE = '.'
+  GALAXY = '#'
+
+  def expand_galaxy(galaxy)
+    galaxy.reduce([]).with_index do |(acc, row), index|
+      # if index.zero? && row.uniq.length == 1
+      #   acc
+      # end
+      row.map.with_index do |space, index|
+        
+      end
+    end
+  end
 
   def self.part1
-    INPUT.sum do |row|
-    end
+    galaxy = INPUT.map(&:strip).map { |row| row.split('') }
+    expanded_galaxy = expand_galaxy(galaxy)
   end
 
   def self.part2
