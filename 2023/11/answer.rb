4345aaed0ae179005dcfa7412937303b65e0c0e9
Tanner Mares <tannermares@gmail.com>
2023-12-14 16:38:44 -0800

    got part 1

--- 2023/11/answer.rb
+++ 2023/11/answer.rb
@@ -3,26 +3,54 @@
 
 # Day 11
 module Day11
-  SAMPLE = true
+  SAMPLE = false
   INPUT_PATH = File.join(File.dirname(__FILE__), SAMPLE ? 'sample.txt' : 'input.txt').freeze
   INPUT = File.readlines(INPUT_PATH)
   EMPTY_SPACE = '.'
   GALAXY = '#'
 
-  def expand_galaxy(galaxy)
-    galaxy.reduce([]).with_index do |(acc, row), index|
-      # if index.zero? && row.uniq.length == 1
-      #   acc
-      # end
-      row.map.with_index do |space, index|
-        
-      end
+  def self.expand_rows(galaxy)
+    galaxy.each_with_object([]) do |row, acc|
+      row.uniq.length == 1 ? 2.times { acc << row } : acc << row
     end
+  end
+
+  def self.expand_columns(galaxy)
+    galaxy.transpose.each_with_object([]) do |row, acc|
+      row.uniq.length == 1 ? 2.times { acc << row } : acc << row
+    end.transpose
+  end
+
+  def self.expand_galaxy(galaxy)
+    expanded_rows = expand_rows(galaxy)
+    expand_columns(expanded_rows)
+  end
+
+  def self.number_galaxies(galaxy)
+    galaxy_number = 0
+    galaxy.map { |row| row.map { |space| space == '#' ? (galaxy_number += 1) : space } }
+  end
+
+  def self.find_coords(galaxy)
+    coords = []
+    galaxy.each_with_index { |row, x| row.each_with_index { |space, y| space != '.' && coords << [x, y] } }
+    coords
+  end
+
+  def self.distance(coord1, coord2)
+    Math.sqrt(((coord2[0] - coord1[0])**2) + ((coord2[1] - coord1[1])**2))
+  end
+
+  def self.grid_distance(coord1, coord2)
+    (coord2[0] - coord1[0]).abs + (coord2[1] - coord1[1]).abs
   end
 
   def self.part1
     galaxy = INPUT.map(&:strip).map { |row| row.split('') }
     expanded_galaxy = expand_galaxy(galaxy)
+    numbered_galaxies = number_galaxies(expanded_galaxy)
+    coords = find_coords(numbered_galaxies)
+    coords.combination(2).sum { |crds| grid_distance(*crds) }
   end
 
   def self.part2
--- 2023/11/answer_test.rb
+++ 2023/11/answer_test.rb
@@ -35,14 +35,38 @@ EXPANDED_GALAXY = [
 
 # Test Day 11
 class TestDay11 < Test::Unit::TestCase
+  def test_expand_rows
+    assert_equal([['.', '.', '.', '.'], ['.', '.', '.', '.']], Day11.expand_rows([['.', '.', '.', '.']]))
+    assert_equal([['.', '.', '#', '.']], Day11.expand_rows([['.', '.', '#', '.']]))
+    assert_equal(
+      [['.', '.', '#', '.'], ['.', '.', '.', '.'], ['.', '.', '.', '.']],
+      Day11.expand_rows([['.', '.', '#', '.'], ['.', '.', '.', '.']])
+    )
+  end
+
+  def test_expand_columns
+    assert_equal([['.', '.'], ['.', '.'], ['.', '.'], ['.', '.']], Day11.expand_columns([['.'], ['.'], ['.'], ['.']]))
+    assert_equal([['.'], ['#'], ['.'], ['.']], Day11.expand_columns([['.'], ['#'], ['.'], ['.']]))
+    assert_equal(
+      [['.', '.', '.'], ['#', '.', '.'], ['.', '.', '.'], ['.', '.', '.']],
+      Day11.expand_columns([['.', '.'], ['#', '.'], ['.', '.'], ['.', '.']])
+    )
+  end
+
   def test_expand_galaxy
-    omit
     assert_equal(EXPANDED_GALAXY, Day11.expand_galaxy(GALAXY))
   end
 
+  def test_grid_distance
+    assert_equal(9, Day11.grid_distance([6, 1], [11, 5]))
+    assert_equal(15, Day11.grid_distance([0, 4], [10, 9]))
+    assert_equal(17, Day11.grid_distance([2, 0], [7, 12]))
+    assert_equal(5, Day11.grid_distance([11, 0], [11, 5]))
+    assert_equal(6, Day11.grid_distance([7, 12], [10, 9]))
+  end
+
   def test_part1
-    omit
-    Day11::SAMPLE ? assert_equal(374, Day11.part1) : assert_equal(nil, Day11.part1)
+    Day11::SAMPLE ? assert_equal(374, Day11.part1) : assert_equal(9_605_127, Day11.part1)
   end
 
   def test_part2
