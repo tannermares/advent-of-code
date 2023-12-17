e4c9d8c42f1dc46ad7b9dc9be79b17e328ec1244
Tanner Mares <tannermares@gmail.com>
2023-12-17 13:14:49 -0800

    my part two is too high... I'll come back

--- 2023/14/answer.rb
+++ 2023/14/answer.rb
@@ -19,20 +19,48 @@ module Day14
     sort_rocks
   end
 
-  def self.part1
-    grid = INPUT.map { |row| row.strip.chars }
-    rolled_grid = grid.transpose.map { |row| sort_rocks_to_front(row.join).split('') }.transpose
+  def self.roll_north(grid)
+    grid.transpose.map { |row| sort_rocks_to_front(row.join).split('') }.transpose
+  end
 
+  def self.roll_west(grid)
+    grid.map { |row| sort_rocks_to_front(row.join).split('') }
+  end
+
+  def self.roll_south(grid)
+    grid.transpose.map { |row| sort_rocks_to_front(row.reverse.join).split('').reverse }.transpose
+  end
+
+  def self.roll_east(grid)
+    grid.map { |row| sort_rocks_to_front(row.reverse.join).split('').reverse }
+  end
+
+  def self.cycle(grid)
+    grid = roll_north(grid)
+    grid = roll_west(grid)
+    grid = roll_south(grid)
+    roll_east(grid)
+  end
+
+  def self.sum_grid(grid)
     sum = 0
-    rolled_grid.each_with_index do |row, index|
-      sum += (row.join.count('O') * (rolled_grid.length - index))
+    grid.each_with_index do |row, index|
+      sum += (row.join.count('O') * (grid.length - index))
     end
-
     sum
   end
 
+  def self.part1
+    grid = INPUT.map { |row| row.strip.chars }
+    rolled_grid = roll_north(grid)
+    sum_grid(rolled_grid)
+  end
+
   def self.part2
-    INPUT.sum do |row|
-    end
+    grid = INPUT.map { |row| row.strip.chars }
+    cycled_grid = nil
+    # 1_000_000_000.times { cycled_grid = cycle(grid) }
+    1.times { cycled_grid = cycle(grid) }
+    sum_grid(cycled_grid)
   end
 end
