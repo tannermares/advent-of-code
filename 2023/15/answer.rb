b517cfc7af40eebde4ca9b0d6b1128a8f59da413
Tanner Mares <tannermares@gmail.com>
2023-12-17 14:03:08 -0800

    day 15 part 1

--- 2023/15/answer.rb
+++ 2023/15/answer.rb
@@ -3,13 +3,20 @@
 
 # Day 15
 module Day15
-  SAMPLE = true
+  SAMPLE = false
   INPUT_PATH = File.join(File.dirname(__FILE__), SAMPLE ? 'sample.txt' : 'input.txt').freeze
   INPUT = File.readlines(INPUT_PATH)
 
-  def self.part1
-    INPUT.sum do |row|
+  def self.hash_sequence(sequence)
+    sequence.chars.reduce(0) do |sum, char|
+      sum += char.ord
+      sum *= 17
+      sum % 256
     end
+  end
+
+  def self.part1
+    INPUT[0].split(',').sum { |sequence| hash_sequence(sequence) }
   end
 
   def self.part2
--- 2023/15/answer_test.rb
+++ 2023/15/answer_test.rb
@@ -7,9 +7,12 @@ require_relative 'answer'
 
 # Test Day 15
 class TestDay15 < Test::Unit::TestCase
+  def test_hash_sequence
+    assert_equal(52, Day15.hash_sequence('HASH'))
+  end
+
   def test_part1
-    omit
-    Day15::SAMPLE ? assert_equal(nil, Day15.part1) : assert_equal(nil, Day15.part1)
+    Day15::SAMPLE ? assert_equal(1_320, Day15.part1) : assert_equal(507_666, Day15.part1)
   end
 
   def test_part2
