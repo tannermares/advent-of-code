526fd9d7eddd601bd7669cd797122267c8e8a28b
Tanner Mares <tannermares@gmail.com>
2023-12-17 17:18:52 -0800

    day 15 part 2

--- 2023/15/answer.rb
+++ 2023/15/answer.rb
@@ -15,12 +15,42 @@ module Day15
     end
   end
 
+  def self.sum_box(box_number, lenses)
+    lenses.each_with_index.reduce(0) do |sum, ((_label, focal_length), index)|
+      sum + (box_number + 1) * (index + 1) * focal_length
+    end
+  end
+
+  def self.sort_lens(boxes, sequence)
+    if sequence.end_with?('-')
+      label = sequence.split('-').first
+      hash = hash_sequence(label)
+      boxes[hash] ||= []
+      boxes[hash] = boxes[hash].reject { |l, _| l == label }
+    else
+      label, focal_length = sequence.split('=')
+      hash = hash_sequence(label)
+      boxes[hash] ||= []
+      existing_index = boxes[hash].index { |l, _| l == label }
+      new_lens = [label, focal_length.to_i]
+
+      if existing_index
+        boxes[hash][existing_index] = new_lens
+      else
+        boxes[hash] << new_lens
+      end
+    end
+
+    boxes
+  end
+
   def self.part1
     INPUT[0].split(',').sum { |sequence| hash_sequence(sequence) }
   end
 
   def self.part2
-    INPUT.sum do |row|
-    end
+    boxes = {}
+    INPUT[0].split(',') { |sequence| sort_lens(boxes, sequence) }
+    boxes.sum { |box_number, lenses| sum_box(box_number, lenses) }
   end
 end
--- 2023/15/answer_test.rb
+++ 2023/15/answer_test.rb
@@ -7,6 +7,18 @@ require_relative 'answer'
 
 # Test Day 15
 class TestDay15 < Test::Unit::TestCase
+  def test_sum_box
+    assert_equal(5, Day15.sum_box(0, [['rn', 1], ['cm', 2]]))
+    assert_equal(140, Day15.sum_box(3, [['ot', 7], ['ab', 5], ['pc', 6]]))
+  end
+
+  def test_sort_lens
+    assert_equal({ 0 => [['rn', 1]] }, Day15.sort_lens({}, 'rn=1'))
+    assert_equal({ 0 => [['rn', 1], ['cm', 2]] }, Day15.sort_lens({ 0 => [['rn', 1]] }, 'cm=2'))
+    assert_equal({ 0 => [['rn', 1]] }, Day15.sort_lens({ 0 => [['rn', 1]] }, 'cm-'))
+    assert_equal({ 0 => [['rn', 1]] }, Day15.sort_lens({ 0 => [['rn', 1], ['cm', 2]] }, 'cm-'))
+  end
+
   def test_hash_sequence
     assert_equal(52, Day15.hash_sequence('HASH'))
   end
@@ -16,7 +28,6 @@ class TestDay15 < Test::Unit::TestCase
   end
 
   def test_part2
-    omit
-    Day15::SAMPLE ? assert_equal(nil, Day15.part2) : assert_equal(nil, Day15.part2)
+    Day15::SAMPLE ? assert_equal(145, Day15.part2) : assert_equal(233_537, Day15.part2)
   end
 end
