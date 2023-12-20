7ba4ba38592440c3c6f59f46428c2306033ede6f
Tanner Mares <tannermares@gmail.com>
2023-12-19 16:21:52 -0800

    added day 18

---
+++ 2023/18/answer_test.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'test/unit'
+require 'stringio'
+require_relative 'answer'
+
+# Test Day 18
+class TestDay18 < Test::Unit::TestCase
+  def test_part1
+    omit
+    Day18::SAMPLE ? assert_equal(nil, Day18.part1) : assert_equal(nil, Day18.part1)
+  end
+
+  def test_part2
+    omit
+    Day18::SAMPLE ? assert_equal(nil, Day18.part2) : assert_equal(nil, Day18.part2)
+  end
+end
