9e504868bac97fd141fab31d62c0eeb15623c0d1
Tanner Mares <tannermares@gmail.com>
2023-12-21 09:26:12 -0800

    can actually generate an answer but it's too low

--- 2023/12/answer.rb
+++ 2023/12/answer.rb
@@ -3,12 +3,39 @@
 
 # Day 12
 module Day12
-  SAMPLE = true
+  SAMPLE = false
   INPUT_PATH = File.join(File.dirname(__FILE__), SAMPLE ? 'sample.txt' : 'input.txt').freeze
   INPUT = File.readlines(INPUT_PATH)
 
+  def self.count_segments(line)
+    line.split('.').map(&:length).reject(&:zero?)
+  end
+
+  def self.find_arrangements(line, combinations, pattern)
+    valid_combinations = []
+
+    while combinations.length.positive?
+      combination = combinations.pop
+      new_line = line.split(/\?+/).zip(combination).flatten.join
+      valid_combinations << new_line if count_segments(new_line) == pattern
+    end
+
+    valid_combinations.length
+  end
+
+  def self.find_combinations(line)
+    segments = line.split('.').reject(&:empty?).flat_map { |p| p.split('#') }.reject(&:empty?)
+    separate_segments = segments.map do |segment|
+      ['#', '.'].repeated_permutation(segment.length).map(&:join)
+    end
+    separate_segments.first.product(*separate_segments.drop(1))
+  end
+
   def self.part1
     INPUT.sum do |row|
+      record, pattern = row.split
+      combinations = find_combinations(record)
+      find_arrangements(record, combinations, pattern.split(',').map(&:to_i))
     end
   end
 
