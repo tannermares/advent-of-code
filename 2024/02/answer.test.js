0100c43e7397e8e7af68ff7b6a7f919d96368ae9
Tanner Mares <tannermares@gmail.com>
2024-12-02 16:38:40 -0800

    added leading 0s to all days (for sorting), added 2024 day 2

---
+++ 2024/02/answer.test.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node --test
+
+const { describe, test } = require('node:test')
+const assert = require('assert')
+const { part1, part2, SAMPLE } = require('./answer.js')
+
+describe('day2', () => {
+  test('part1', (t) => {
+    t.skip()
+    return
+    SAMPLE
+      ? assert.strictEqual(null, part1())
+      : assert.strictEqual(null, part1())
+  })
+
+  test('part2', (t) => {
+    t.skip()
+    return
+    SAMPLE
+      ? assert.strictEqual(null, part2())
+      : assert.strictEqual(null, part2())
+  })
+})
---
+++ 2024/02/answer_test.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'test/unit'
+require 'stringio'
+require_relative 'answer'
+
+# Test Day 2
+class TestDay2 < Test::Unit::TestCase
+  def test_part1
+    omit
+    Day2::SAMPLE ? assert_equal(nil, Day2.part1) : assert_equal(nil, Day2.part1)
+  end
+
+  def test_part2
+    omit
+    Day2::SAMPLE ? assert_equal(nil, Day2.part2) : assert_equal(nil, Day2.part2)
+  end
+end
