cf2c6823a6be81790456a26e351bd6cdc11976bb
Tanner Mares <tannermares@gmail.com>
2024-12-03 21:32:47 -0800

    handle multi-line input

--- 2024/03/answer.rb
+++ 2024/03/answer.rb
@@ -8,25 +8,29 @@ module Day3
   INPUT = File.readlines(INPUT_PATH)
 
   def self.part1
-    INPUT[0].scan(/mul\((\d{1,3}),(\d{1,3})\)/).map do |(first, second)|
-      first.to_i * second.to_i
+    INPUT.flat_map do |row|
+      row.scan(/mul\((\d{1,3}),(\d{1,3})\)/).map do |(first, second)|
+        first.to_i * second.to_i
+      end
     end.sum
   end
 
   def self.part2
     enabled = true
-    INPUT[0].scan(/(don't\(\)|do\(\)|mul\((\d{1,3}),(\d{1,3})\))/).map do |(command, first, second)|
-      if command == 'do()'
-        enabled = true
-        next 0
-      end
+    INPUT.flat_map do |row|
+      row.scan(/(don't\(\)|do\(\)|mul\((\d{1,3}),(\d{1,3})\))/).map do |(command, first, second)|
+        if command == 'do()'
+          enabled = true
+          next 0
+        end
 
-      if command == "don't()"
-        enabled = false
-        next 0
-      end
+        if command == "don't()"
+          enabled = false
+          next 0
+        end
 
-      enabled ? first.to_i * second.to_i : 0
+        enabled ? first.to_i * second.to_i : 0
+      end
     end.sum
   end
 end
