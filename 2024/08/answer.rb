482527169033d1da27ad0e4988c29ef4d126038b
Tanner Mares <tannermares@gmail.com>
2024-12-08 12:38:00 -0800

    got part 2 in ruby

--- 2024/08/answer.rb
+++ 2024/08/answer.rb
@@ -3,7 +3,7 @@
 
 # Day 08
 module Day08
-  SAMPLE = false
+  SAMPLE = true
   INPUT_PATH = File.join(File.dirname(__FILE__), SAMPLE ? 'sample.txt' : 'input.txt').freeze
   INPUT = File.readlines(INPUT_PATH)
   GRID = INPUT.map { |row| row.strip.chars }
@@ -11,6 +11,14 @@ module Day08
   WIDTH = GRID.first.length
 
   def self.part1
+    find_antinodes(find_resonant_harmonics: false)
+  end
+
+  def self.part2
+    find_antinodes(find_resonant_harmonics: true)
+  end
+
+  def self.find_antinodes(find_resonant_harmonics:)
     frequencies = {}
     antinodes = []
 
@@ -26,7 +34,7 @@ module Day08
     frequencies.each_value do |nodes|
       nodes[:pairs] = nodes[:coords].combination(2).to_a
       nodes[:pairs].each do |pair|
-        new_antinodes = find_antinodes(pair[0], pair[1])
+        new_antinodes = find_antinodes_for_pair(pair[0], pair[1], find_resonant_harmonics: find_resonant_harmonics)
         nodes[:antinodes] += new_antinodes
         antinodes += new_antinodes
       end
@@ -35,12 +43,7 @@ module Day08
     antinodes.uniq.filter { |a| valid_antinode?(a) }.count
   end
 
-  def self.part2
-    INPUT.sum do |row|
-    end
-  end
-
-  def self.find_antinodes(node, other_node)
+  def self.find_antinodes_for_pair(node, other_node, find_resonant_harmonics: false)
     x1, y1 = node
     x2, y2 = other_node
 
@@ -48,7 +51,21 @@ module Day08
     dx = x2 - x1
     dy = y2 - y1
 
-    [[x1 - dx, y1 - dy], [x2 + dx, y2 + dy]]
+    new_start = [x1 - dx, y1 - dy]
+    new_end = [x2 + dx, y2 + dy]
+    antinodes = [new_start, new_end]
+
+    if find_resonant_harmonics
+      antinodes += [node, other_node]
+
+      while valid_antinode?(new_start) || valid_antinode?(new_end)
+        new_start = [new_start[0] - dx, new_start[1] - dy]
+        new_end = [new_end[0] + dx, new_end[1] + dy]
+        antinodes += [new_start, new_end]
+      end
+    end
+
+    antinodes
   end
 
   def self.valid_antinode?(antinode)
--- 2024/08/answer_test.rb
+++ 2024/08/answer_test.rb
@@ -7,16 +7,11 @@ require_relative 'answer'
 
 # Test Day 08
 class TestDay08 < Test::Unit::TestCase
-
-  # 316 too low
-  # 348
-  # 349 too high
   def test_part1
     Day08::SAMPLE ? assert_equal(14, Day08.part1) : assert_equal(348, Day08.part1)
   end
 
   def test_part2
-    omit
-    Day08::SAMPLE ? assert_equal(nil, Day08.part2) : assert_equal(nil, Day08.part2)
+    Day08::SAMPLE ? assert_equal(34, Day08.part2) : assert_equal(nil, Day08.part2)
   end
 end
