36b97fb8d241b932ce6b9662b4774e2fb039f154
Tanner Mares <tannermares@gmail.com>
2024-12-11 17:32:43 -0800

    updated readme

--- 2024/11/answer.rb
+++ 2024/11/answer.rb
@@ -9,28 +9,19 @@ module Day11
   STONES = INPUT.first.split(' ').map(&:to_i)
 
   def self.part1
-    stones = Hash.new(0)
-    STONES.each { |stone| stones[stone] += 1 }
-    blink_cache = {}
-
-    25.times do |n|
-      next_stones = Hash.new(0)
-      stones.each do |stone, count|
-        results = blink(stone, blink_cache)
-        Array(results).each { |result| next_stones[result] += count }
-        stones = next_stones
-      end
-    end
-
-    stones.values.sum
+    blink_times(25)
   end
 
   def self.part2
+    blink_times(75)
+  end
+
+  def self.blink_times(number)
     stones = Hash.new(0)
     STONES.each { |stone| stones[stone] += 1 }
     blink_cache = {}
 
-    75.times do |n|
+    number.times do
       next_stones = Hash.new(0)
       stones.each do |stone, count|
         results = blink(stone, blink_cache)
