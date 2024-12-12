d1f5631f8dfb811de37221c8991b4926f840a6c9
Tanner Mares <tannermares@gmail.com>
2024-12-11 19:16:35 -0800

    ported dat 10 to JS

--- 2024/10/answer.js
+++ 2024/10/answer.js
@@ -7,13 +7,87 @@ const input = fs
   .readFileSync(path.resolve(__dirname, SAMPLE ? 'sample.txt' : 'input.txt'))
   .toString()
   .split('\n')
+const grid = input.map((row) => row.split('').map((cell) => parseInt(cell)))
+const height = grid.length
+const width = grid[0].length
+const directions = [
+  [0, -1],
+  [1, 0],
+  [0, 1],
+  [-1, 0],
+]
 
 function part1() {
-  return input.map((row) => {}).reduce((acc, n) => (acc += n))
+  let score = 0
+
+  grid.forEach((row, i) => {
+    row.forEach((cell, j) => {
+      if (cell === 0) {
+        score += walkTrail(0, [j, i], Set).size
+      }
+    })
+  })
+
+  return score
 }
 
 function part2() {
-  return input.map((row) => {}).reduce((acc, n) => (acc += n))
+  let score = 0
+
+  grid.forEach((row, i) => {
+    row.forEach((cell, j) => {
+      if (cell === 0) {
+        score += walkTrail(0, [j, i], Array).length
+      }
+    })
+  })
+
+  return score
+}
+
+function walkTrail(number, position, collection) {
+  let summits = new collection()
+  let newDirections = [...directions]
+
+  while (newDirections.length > 0) {
+    let direction = newDirections.shift()
+    let nextPosition = position.map((pos, index) => pos + direction[index])
+    if (!validPosition(nextPosition)) continue
+
+    let nextNumber = grid[nextPosition[1]][nextPosition[0]]
+    if (nextNumber !== number + 1) continue
+
+    if (nextNumber === 9) {
+      if (Array.isArray(summits)) {
+        summits.push(nextPosition)
+      } else {
+        summits.add(JSON.stringify(nextPosition))
+      }
+    } else {
+      if (Array.isArray(summits)) {
+        summits = [
+          ...summits,
+          ...walkTrail(nextNumber, nextPosition, collection),
+        ]
+      } else {
+        summits = new collection([
+          ...summits,
+          ...walkTrail(nextNumber, nextPosition, collection),
+        ])
+      }
+    }
+  }
+
+  return summits
+}
+
+function validPosition(position) {
+  return (
+    position[0] >= 0 &&
+    position[0] <= width - 1 &&
+    position[1] >= 0 &&
+    position[1] <= height - 1
+  )
 }
 
 module.exports = { part1, part2, SAMPLE }
--- 2024/10/answer.test.js
+++ 2024/10/answer.test.js
@@ -6,18 +6,14 @@ const { part1, part2, SAMPLE } = require('./answer.js')
 
 describe('day10', () => {
   test('part1', (t) => {
-    t.skip()
-    return
-    SAMPLE
-      ? assert.strictEqual(part1(), null)
-      : assert.strictEqual(part1(), null)
+    return SAMPLE
+      ? assert.strictEqual(part1(), 36)
+      : assert.strictEqual(part1(), 510)
   })
 
   test('part2', (t) => {
-    t.skip()
-    return
-    SAMPLE
-      ? assert.strictEqual(part2(), null)
-      : assert.strictEqual(part2(), null)
+    return SAMPLE
+      ? assert.strictEqual(part2(), 81)
+      : assert.strictEqual(part2(), 1058)
   })
 })
