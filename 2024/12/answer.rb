871d6a62b9f1300c1f501fecc22a37af732bef5c
Tanner Mares <tannermares@gmail.com>
2024-12-13 19:22:59 -0800

    real input isn't on github need to push with sample true

--- 2024/12/answer.rb
+++ 2024/12/answer.rb
@@ -3,7 +3,7 @@
 
 # Day 12
 module Day12
-  SAMPLE = false
+  SAMPLE = true
   INPUT_PATH = File.join(File.dirname(__FILE__), SAMPLE ? 'sample.txt' : 'input.txt').freeze
   INPUT = File.readlines(INPUT_PATH)
   GRID = INPUT.map { |row| row.strip.chars }
