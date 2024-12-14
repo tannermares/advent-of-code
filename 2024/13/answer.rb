e7fb04c88cc5ec44848da9774290a959a73a72f7
Tanner Mares <tannermares@gmail.com>
2024-12-14 15:58:27 -0800

    got part 2 day 13

--- 2024/13/answer.rb
+++ 2024/13/answer.rb
@@ -12,51 +12,88 @@ module Day13
     current_machine = {}
 
     INPUT.each do |row|
-      if row == "\n"
-        machines << current_machine
-        current_machine = {}
-        next
+      next if row == "\n"
+
+      if current_machine[:a].nil?
+        button_a = row.scan(/Button A: X\+(\d+), Y\+(\d+)/)[0]
+        next current_machine[:a] = button_a.map(&:to_i) if button_a
       end
 
-      button_a = row.scan(/Button A: X\+(\d+), Y\+(\d+)/)[0]
-      next current_machine[:a] = button_a.map(&:to_i) if button_a
+      if current_machine[:b].nil?
+        button_b = row.scan(/Button B: X\+(\d+), Y\+(\d+)/)[0]
+        next current_machine[:b] = button_b.map(&:to_i) if button_b
+      end
 
-      button_b = row.scan(/Button B: X\+(\d+), Y\+(\d+)/)[0]
-      next current_machine[:b] = button_b.map(&:to_i) if button_b
-
-      prize = row.scan(/Prize: X=(\d+), Y=(\d+)/)[0]
-      current_machine[:prize] = prize.map(&:to_i) if prize
+      if current_machine[:prize].nil?
+        prize = row.scan(/Prize: X=(\d+), Y=(\d+)/)[0]
+        if prize
+          current_machine[:prize] = prize.map(&:to_i)
+          machines << current_machine
+          current_machine = {}
+        end
+      end
     end
 
     machines.sum do |machine|
-      x_num = (machine[:prize][0] * machine[:b][1]) + (machine[:prize][1] * -machine[:b][0])
-      x_denom = (machine[:a][0] * machine[:b][1]) + (machine[:a][1] * -machine[:b][0])
+      x_num = (machine[:prize][0] * machine[:b][1]) - (machine[:prize][1] * machine[:b][0])
+      x_denom = (machine[:a][0] * machine[:b][1]) - (machine[:a][1] * machine[:b][0])
       x = x_num / x_denom
-
-      x2_num = (machine[:prize][1] * machine[:b][0]) + (machine[:prize][0] * -machine[:b][1])
-      x2_denom = (machine[:a][1] * machine[:b][0]) + (machine[:a][0] * -machine[:b][1])
-      x2 = x2_num / x2_denom
-      # next 0 unless x == x2
 
       y_num = machine[:prize][0] - (machine[:a][0] * x)
       y_denom = machine[:b][0]
       y = y_num / y_denom
 
-      y2_num = machine[:prize][1] - (machine[:a][1] * x2)
-      y2_denom = machine[:b][1]
-      y2 = y2_num / y2_denom
-      # next 0 unless y == y2
+      proved = (machine[:a][0] * x) + (machine[:b][0] * y) == machine[:prize][0] &&
+               (machine[:a][1] * x) + (machine[:b][1] * y) == machine[:prize][1]
 
-      proved1 = (machine[:a][0] * x) + (machine[:b][0] * y) == machine[:prize][0]
-      proved2 = (machine[:a][1] * x2) + (machine[:b][1] * y2) == machine[:prize][1]
-      next 0 unless proved1 && proved2
+      next 0 unless proved
 
       (x * 3) + (y * 1)
     end
   end
 
   def self.part2
-    INPUT.sum do |row|
+    machines = []
+    current_machine = {}
+
+    INPUT.each do |row|
+      next if row == "\n"
+
+      if current_machine[:a].nil?
+        button_a = row.scan(/Button A: X\+(\d+), Y\+(\d+)/)[0]
+        next current_machine[:a] = button_a.map(&:to_i) if button_a
+      end
+
+      if current_machine[:b].nil?
+        button_b = row.scan(/Button B: X\+(\d+), Y\+(\d+)/)[0]
+        next current_machine[:b] = button_b.map(&:to_i) if button_b
+      end
+
+      if current_machine[:prize].nil?
+        prize = row.scan(/Prize: X=(\d+), Y=(\d+)/)[0]
+        if prize
+          current_machine[:prize] = prize.map(&:to_i).map { |n| n + 10_000_000_000_000 }
+          machines << current_machine
+          current_machine = {}
+        end
+      end
+    end
+
+    machines.sum do |machine|
+      x_num = (machine[:prize][0] * machine[:b][1]) - (machine[:prize][1] * machine[:b][0])
+      x_denom = (machine[:a][0] * machine[:b][1]) - (machine[:a][1] * machine[:b][0])
+      x = x_num / x_denom
+
+      y_num = machine[:prize][0] - (machine[:a][0] * x)
+      y_denom = machine[:b][0]
+      y = y_num / y_denom
+
+      proved = (machine[:a][0] * x) + (machine[:b][0] * y) == machine[:prize][0] &&
+               (machine[:a][1] * x) + (machine[:b][1] * y) == machine[:prize][1]
+
+      next 0 unless proved
+
+      (x * 3) + (y * 1)
     end
   end
 end
--- 2024/13/answer_test.rb
+++ 2024/13/answer_test.rb
@@ -7,15 +7,11 @@ require_relative 'answer'
 
 # Test Day 13
 class TestDay13 < Test::Unit::TestCase
-  # 31_351 too low
-  # 31_781 too high
-  # 32_003
   def test_part1
-    Day13::SAMPLE ? assert_equal(480, Day13.part1) : assert_equal(nil, Day13.part1)
+    Day13::SAMPLE ? assert_equal(480, Day13.part1) : assert_equal(31_623, Day13.part1)
   end
 
   def test_part2
-    omit
-    Day13::SAMPLE ? assert_equal(nil, Day13.part2) : assert_equal(nil, Day13.part2)
+    Day13::SAMPLE ? assert_equal(875_318_608_908, Day13.part2) : assert_equal(93_209_116_744_825, Day13.part2)
   end
 end
