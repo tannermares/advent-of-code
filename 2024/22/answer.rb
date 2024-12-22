06a7c1e503260f661b4162b0c64af7011a4b5e8e
Tanner Mares <tannermares@gmail.com>
2024-12-21 22:49:40 -0800

    got part 1 day 22

--- 2024/22/answer.rb
+++ 2024/22/answer.rb
@@ -9,11 +9,57 @@ module Day22
 
   def self.part1
     INPUT.sum do |row|
+      secret = row.to_i
+      2000.times { secret = generate_secret(secret) }
+      secret
     end
   end
 
   def self.part2
+    changes = Hash.new([])
+
     INPUT.sum do |row|
+      secret = row.to_i
+
+      2000.times do
+        prev_secret = secret
+        secret = generate_secret(secret)
+
+        prev_ones = prev_secret.to_s.chars.last.to_i
+        ones = secret.to_s.chars.last.to_i
+
+        changes[row] << { price: ones, change: ones - prev_ones }
+      end
+      0
     end
+  end
+
+  def self.generate_secret(number)
+    first = step1(number)
+    second = step2(first)
+    step3(second)
+  end
+
+  def self.step1(number)
+    mixed = mix(number * 64, number)
+    prune(mixed)
+  end
+
+  def self.step2(number)
+    mixed = mix(number / 32, number)
+    prune(mixed)
+  end
+
+  def self.step3(number)
+    mixed = mix(number * 2048, number)
+    prune(mixed)
+  end
+
+  def self.mix(value, secret)
+    value ^ secret
+  end
+
+  def self.prune(secret)
+    secret % 16_777_216
   end
 end
--- 2024/22/answer_test.rb
+++ 2024/22/answer_test.rb
@@ -8,12 +8,11 @@ require_relative 'answer'
 # Test Day 22
 class TestDay22 < Test::Unit::TestCase
   def test_part1
-    omit
-    Day22::SAMPLE ? assert_equal(nil, Day22.part1) : assert_equal(nil, Day22.part1)
+    Day22::SAMPLE ? assert_equal(37_327_623, Day22.part1) : assert_equal(14_476_723_788, Day22.part1)
   end
 
   def test_part2
     omit
-    Day22::SAMPLE ? assert_equal(nil, Day22.part2) : assert_equal(nil, Day22.part2)
+    Day22::SAMPLE ? assert_equal(23, Day22.part2) : assert_equal(nil, Day22.part2)
   end
 end
