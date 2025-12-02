d4ba2020b95a630b43cdb8ff21e5a851b88b037c
Tanner Mares <tannermares@gmail.com>
2025-12-01 18:53:25 -0800

    got day 1 ruby working

--- 2025/01/answer.rb
+++ 2025/01/answer.rb
@@ -3,17 +3,40 @@
 
 # Day 01
 module Day01
-  SAMPLE = true
+  SAMPLE = false
   INPUT_PATH = File.join(File.dirname(__FILE__), SAMPLE ? 'sample.txt' : 'input.txt').freeze
   INPUT = File.readlines(INPUT_PATH)
 
   def self.part1
-    INPUT.sum do |row|
+    current_position = 50
+
+    INPUT.count do |row|
+      new_row = row.dup
+      direction = new_row.slice!(0)
+      amount = direction == 'R' ? new_row.to_i : -new_row.to_i
+      new_position = current_position + amount
+      current_position = new_position % 100
+      current_position.zero?
     end
   end
 
   def self.part2
-    INPUT.sum do |row|
+    current_position = 50
+    zeros = 0
+
+    INPUT.each do |row|
+      new_row = row.dup
+      direction = new_row.slice!(0)
+      amount = new_row.to_i
+      operator = direction == 'R' ? :+ : :-
+
+      amount.times do |_|
+        current_position = current_position.send(operator, 1)
+        current_position %= 100
+        zeros += 1 if current_position.zero?
+      end
     end
+
+    zeros
   end
 end
--- 2025/01/answer_test.rb
+++ 2025/01/answer_test.rb
@@ -8,12 +8,10 @@ require_relative 'answer'
 # Test Day 01
 class TestDay01 < Test::Unit::TestCase
   def test_part1
-    omit
-    Day01::SAMPLE ? assert_equal(nil, Day01.part1) : assert_equal(nil, Day01.part1)
+    Day01::SAMPLE ? assert_equal(3, Day01.part1) : assert_equal(982, Day01.part1)
   end
 
   def test_part2
-    omit
-    Day01::SAMPLE ? assert_equal(nil, Day01.part2) : assert_equal(nil, Day01.part2)
+    Day01::SAMPLE ? assert_equal(6, Day01.part2) : assert_equal(6_106, Day01.part2)
   end
 end
