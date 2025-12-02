39755cf6fbbe911ce3b2d63ab1ae34bf03f5fb4d
Tanner Mares <tannermares@gmail.com>
2025-12-01 21:03:22 -0800

    added day 2 template

---
+++ 2025/02/answer_test.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'test/unit'
+require 'stringio'
+require_relative 'answer'
+
+# Test Day 02
+class TestDay02 < Test::Unit::TestCase
+  def test_part1
+    omit
+    Day02::SAMPLE ? assert_equal(nil, Day02.part1) : assert_equal(nil, Day02.part1)
+  end
+
+  def test_part2
+    omit
+    Day02::SAMPLE ? assert_equal(nil, Day02.part2) : assert_equal(nil, Day02.part2)
+  end
+end
