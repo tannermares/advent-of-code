01701a95fb025e13c782137951baa519a9dafab1
Tanner Mares <tannermares@gmail.com>
2025-12-01 23:30:22 -0800

    got day 2 in JS

--- 2025/02/answer.js
+++ 2025/02/answer.js
@@ -9,11 +9,65 @@ const input = fs
   .split('\n')
 
 function part1() {
-  return input.map((row) => {}).reduce((acc, n) => (acc += n))
+  return input[0]
+    .split(',')
+    .map((row) => {
+      let invalid = []
+      const [low, high] = row.split('-').map((n) => parseInt(n))
+      const range = Array.from({ length: high - low + 1 }, (_, i) => low + i)
+
+      range.forEach((n) => {
+        const nString = `${n}`
+        const length = nString.length
+
+        mid = length / 2
+        first = nString.slice(0, mid)
+        second = nString.slice(mid)
+
+        if (first == second) invalid.push(n)
+      })
+
+      return invalid.length > 0 ? invalid.reduce((acc, n) => (acc += n)) : 0
+    })
+    .reduce((acc, n) => (acc += n))
 }
 
 function part2() {
-  return input.map((row) => {}).reduce((acc, n) => (acc += n))
+  return input[0]
+    .split(',')
+    .map((row) => {
+      let invalid = []
+      const [low, high] = row.split('-').map((n) => parseInt(n))
+      const range = Array.from({ length: high - low + 1 }, (_, i) => low + i)
+
+      range.forEach((n) => {
+        const nString = `${n}`
+        const length = nString.length
+
+        mid = Math.floor(length / 2)
+        first = nString.slice(0, mid)
+        second = nString.slice(mid)
+
+        for (let i = mid; i > 0; i -= 1) {
+          let chunks = []
+
+          for (let j = 0; j < nString.length; j += i) {
+            const chunk = nString.slice(j, j + i)
+            chunks.push(chunk)
+          }
+
+          let unique = [...new Set(chunks)]
+
+          if (unique.length == 1) {
+            invalid.push(n)
+            break
+          }
+        }
+      })
+
+      return invalid.length > 0 ? invalid.reduce((acc, n) => (acc += n)) : 0
+    })
+    .reduce((acc, n) => (acc += n))
 }
 
 module.exports = { part1, part2, SAMPLE }
--- 2025/02/answer.test.js
+++ 2025/02/answer.test.js
@@ -5,19 +5,15 @@ const assert = require('assert')
 const { part1, part2, SAMPLE } = require('./answer.js')
 
 describe('day02', () => {
-  test('part1', (t) => {
-    t.skip()
-    return
+  test('part1', () => {
     SAMPLE
-      ? assert.strictEqual(part1(), null)
-      : assert.strictEqual(part1(), null)
+      ? assert.strictEqual(part1(), 1_227_775_554)
+      : assert.strictEqual(part1(), 53_420_042_388)
   })
 
-  test('part2', (t) => {
-    t.skip()
-    return
+  test('part2', () => {
     SAMPLE
-      ? assert.strictEqual(part2(), null)
-      : assert.strictEqual(part2(), null)
+      ? assert.strictEqual(part2(), 4_174_379_265)
+      : assert.strictEqual(part2(), 69_553_832_684)
   })
 })
