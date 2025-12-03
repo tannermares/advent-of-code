790f77ae33dcc08c01f9ae51f607ec6be370595c
Tanner Mares <tannermares@gmail.com>
2025-12-03 00:46:52 -0800

    got day 3 in JS

--- 2025/03/answer.js
+++ 2025/03/answer.js
@@ -9,11 +9,38 @@ const input = fs
   .split('\n')
 
 function part1() {
-  return input.map((row) => {}).reduce((acc, n) => (acc += n))
+  return input
+    .map((row) => largestNumber(row.trim().split(''), 1))
+    .reduce((acc, n) => (acc += n))
 }
 
 function part2() {
-  return input.map((row) => {}).reduce((acc, n) => (acc += n))
+  return input
+    .map((row) => largestNumber(row.trim().split(''), 11))
+    .reduce((acc, n) => (acc += n))
+}
+
+function largestNumber(chars, digit) {
+  const digits = []
+
+  while (digit >= 0) {
+    const index = chars.indexOf(`${digits[digits.length - 1]}`)
+    if (index !== -1) chars = chars.slice(index + 1)
+
+    digits.push(firstBiggest(chars, digit))
+    digit = digit - 1
+  }
+
+  return parseInt(digits.join(''))
+}
+
+function firstBiggest(array, digit) {
+  for (i = 9; i > 0; i--) {
+    const index = array.indexOf(`${i}`)
+    if (index === -1 || index >= array.length - digit) continue
+
+    return i
+  }
 }
 
 module.exports = { part1, part2, SAMPLE }
--- 2025/03/answer.test.js
+++ 2025/03/answer.test.js
@@ -5,19 +5,15 @@ const assert = require('assert')
 const { part1, part2, SAMPLE } = require('./answer.js')
 
 describe('day03', () => {
-  test('part1', (t) => {
-    t.skip()
-    return
+  test('part1', () => {
     SAMPLE
-      ? assert.strictEqual(part1(), null)
-      : assert.strictEqual(part1(), null)
+      ? assert.strictEqual(part1(), 357)
+      : assert.strictEqual(part1(), 17_403)
   })
 
-  test('part2', (t) => {
-    t.skip()
-    return
+  test('part2', () => {
     SAMPLE
-      ? assert.strictEqual(part2(), null)
-      : assert.strictEqual(part2(), null)
+      ? assert.strictEqual(part2(), 3_121_910_778_619)
+      : assert.strictEqual(part2(), 173_416_889_848_394)
   })
 })
