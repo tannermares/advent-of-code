de07747c03293d75709528ef09a305c851541fb9
Tanner Mares <tannermares@gmail.com>
2025-12-05 00:42:55 -0800

    got day 5 in JS

--- 2025/05/answer.js
+++ 2025/05/answer.js
@@ -9,11 +9,51 @@ const input = fs
   .split('\n')
 
 function part1() {
-  return input.map((row) => {}).reduce((acc, n) => (acc += n))
+  let fresh = 0
+  const ranges = []
+
+  input.forEach((row) => {
+    if (row.includes('-')) {
+      const [first, last] = row.split('-').map((n) => parseInt(n))
+      ranges.push([first, last])
+    } else {
+      number = parseInt(row.trim())
+      if (ranges.some(([f, l]) => number >= f && number <= l)) fresh += 1
+    }
+  })
+
+  return fresh
 }
 
 function part2() {
-  return input.map((row) => {}).reduce((acc, n) => (acc += n))
+  const ranges = []
+
+  for (let i = 0; i < input.length; i++) {
+    if (input[i].trim().length === 0) break
+
+    const [first, last] = input[i].split('-').map((n) => parseInt(n))
+    ranges.push([first, last])
+  }
+
+  const newRanges = mergeOverlappingRanges(ranges)
+  return newRanges.reduce((acc, [f, l]) => (acc += l + 1 - f), 0)
+}
+
+function mergeOverlappingRanges(ranges) {
+  const newRanges = ranges.sort((a, b) => a[0] - b[0])
+  let merged = [newRanges.shift()]
+
+  newRanges.forEach((current) => {
+    const lastMerged = merged[merged.length - 1]
+
+    if (lastMerged[1] >= current[0]) {
+      lastMerged[1] = Math.max(...[lastMerged[1], current[1]])
+    } else {
+      merged.push(current)
+    }
+  })
+
+  return merged
 }
 
 module.exports = { part1, part2, SAMPLE }
--- 2025/05/answer.test.js
+++ 2025/05/answer.test.js
@@ -5,19 +5,13 @@ const assert = require('assert')
 const { part1, part2, SAMPLE } = require('./answer.js')
 
 describe('day05', () => {
-  test('part1', (t) => {
-    t.skip()
-    return
-    SAMPLE
-      ? assert.strictEqual(part1(), null)
-      : assert.strictEqual(part1(), null)
+  test('part1', () => {
+    SAMPLE ? assert.strictEqual(part1(), 3) : assert.strictEqual(part1(), 770)
   })
 
-  test('part2', (t) => {
-    t.skip()
-    return
+  test('part2', () => {
     SAMPLE
-      ? assert.strictEqual(part2(), null)
-      : assert.strictEqual(part2(), null)
+      ? assert.strictEqual(part2(), 14)
+      : assert.strictEqual(part2(), 357_674_099_117_260)
   })
 })
